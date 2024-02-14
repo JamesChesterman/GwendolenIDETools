6a23b00f9eedcbb18312209c455646677c7c63eb
James Chesterman <chesterman261@sky.com>
2024-02-14 11:37:54 +0000

    Can delete selected row in table

--- src/main/java/GwendolenToolWindow/BreakpointsViewer.java
+++ src/main/java/GwendolenToolWindow/BreakpointsViewer.java
@@ -6,10 +6,13 @@ import com.intellij.ui.table.JBTable;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BreakpointsViewer extends JPanel {
-    JLabel tableTitle;
+    DefaultTableModel model;
     JBTable table;
+    JButton deleteButton;
 
 
     public BreakpointsViewer(GwenToolWindowContent gwenToolWindowContent){
@@ -18,17 +21,32 @@ public class BreakpointsViewer extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         makeTable();
+        makeDeleteButton();
     }
 
     private void makeTable(){
         Object[][] data = new Object[][]{{"Agent1", "Has belief: ..."}, {"Agent2", "Has belief..."}, {"Agent3", "Has had ... steps processed"}, {"G", "1"}, {"H", "2"}, {"l", "3"}, {"p", "1"}, {"1", "l"}};
         String[] colNames = new String[]{"Agent Name", "Condition"};
-        DefaultTableModel model = new DefaultTableModel(data, colNames);
+        model = new DefaultTableModel(data, colNames);
         table = new JBTable(model);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         JBScrollPane tableScrollPane = new JBScrollPane(table);
         tableScrollPane.setPreferredSize(new Dimension(this.getWidth(), this.getHeight() / 3));
         add(tableScrollPane);
+    }
+
+    private void makeDeleteButton(){
+        deleteButton = new JButton("Delete Selected Row");
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int rowNum = table.getSelectedRow();
+                if(rowNum != 1){
+                    model.removeRow(rowNum);
+                }
+            }
+        });
+        add(deleteButton);
     }
 }
